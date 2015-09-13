19e794bc705ca74e368b95a87d856d48bdd06346
Dr. Craig A. Counterman <ccount@dimensionofmind.com>
2015-09-13 06:57:31 -0400

    progress on first three plots

---
+++ plot1.R
@@ -0,0 +1,22 @@
+# common code to load the dates in question
+full <-
+  read.table(
+    "household_power_consumption.txt", header = TRUE, sep = ";",na.strings =
+      "?"
+  )
+dates <- as.Date(full$Date, "%d/%m/%Y")
+datetimes <-
+  strptime(paste(full$Date, full$Time), format = "%d/%m/%Y %H:%M:%S", tz =
+             "UTC")
+full$POSIXlt <- datetimes
+ourdata <-
+  subset(full, ((datetimes$year == 107) &
+                  (datetimes$mon == 1) &
+                  ((datetimes$mday == 1) | (datetimes$mday == 2)
+                  )))
+# end common code to load the dates in question
+
+
+png(filename="plot1.png")
+hist(ourdata$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,32 @@
+# common code to load the dates in question
+full <-
+  read.table(
+    "household_power_consumption.txt", header = TRUE, sep = ";",na.strings =
+      "?"
+  )
+dates <- as.Date(full$Date, "%d/%m/%Y")
+datetimes <-
+  strptime(paste(full$Date, full$Time), format = "%d/%m/%Y %H:%M:%S", tz =
+             "UTC")
+full$POSIXlt <- datetimes
+ourdata <-
+  subset(full, ((datetimes$year == 107) &
+                  (datetimes$mon == 1) &
+                  ((datetimes$mday == 1) | (datetimes$mday == 2)
+                  )))
+# end common code to load the dates in question
+
+
+png(filename="plot2.png")
+plot(ourdata$POSIXlt, ourdata$Global_active_power, type="l", xlab ="", ylab="Global Active Power (kilowatts)")
+dev.off()
+png(filename="plot3.png")
+plot(ourdata$POSIXlt, ourdata$Sub_metering_1, type="l", xlab ="", ylab="Energy sub metering")
+lines(ourdata$POSIXlt, ourdata$Sub_metering_2, col="red")
+lines(ourdata$POSIXlt, ourdata$Sub_metering_3, col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "green"), lty=c(1,1,1))
+dev.off()
+
+
+  
+  
